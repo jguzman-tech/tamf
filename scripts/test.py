f3a39eebfd4b7537b1f02d147d75a89c9923d52f
Joseph Guzman <josephfguzman@gmail.com>
2021-03-11 10:53:17 -0700

    added naive algorithm

--- scripts/test.py
+++ scripts/test.py
@@ -6,5 +6,8 @@ if __name__ == '__main__':
     my_model.create_grid_category_bar_chart(
         './plots/harding_and_union_grid_category_bar_chart.png'
     )
+    print("NAIVE:")
+    my_model.solve(5, tamf_algorithm.NAIVE)
+    print("EOC_BINARY_DECISIONS:")
     my_model.solve(5, tamf_algorithm.EOC_BINARY_DECISIONS)
     print("Done!")
